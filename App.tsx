098505e663ade9f39e65d4e86f82c143810a6222
Fabian-LG <100322206+Fabian-LG@users.noreply.github.com>
2023-04-11 13:56:23 -0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+
+import 'react-native-gesture-handler'
+import React from 'react';
+import {SafeAreaView,StatusBar,useColorScheme, View} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import ProductScreen from './src/screens/ProductScreen';
+import ShoppingCartScreen from './src/screens/ShoppingCartScreen';
+import AddressScreen from './src/screens/AddressScreen';
+import Router from './src/router';
+
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig);
+
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
+  };
+
+  return (
+    <View style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content':'dark-content'} />
+      <Router />
+    </View>
+  );
+};
+
+export default App;
